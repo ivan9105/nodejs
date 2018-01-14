fa23ae2601ec2e47b4d1e0f42f4e32967c27e08f
ivan9105 <ivan9105@bk.ru>
2018-01-14 15:35:17 +0400

    mongo 4

--- mongo/basic.js
+++ mongo/basic.js
@@ -21,6 +21,16 @@ function insertOneUsers(dbase, random) {
     });
 }
 
+function insertOrder(dbase, product_id, status) {
+    let obj = {product_id: product_id, status: status};
+    dbase.collection("orders").insertOne(obj, function (error, res) {
+        if (error) throw error;
+        console.log("Insert 1 obj");
+        console.log(res);
+    });
+}
+
+
 function firstTimeout() {
     MongoClient.connect(url, function (error, db) {
         if (error) throw error;
@@ -39,6 +49,7 @@ function firstTimeout() {
         insertOneCustomers(dbase, true);
         insertOneCustomers(dbase, true);
         insertOneCustomers(dbase, false);
+        insertOneCustomers(dbase, false);
 
         dbase.collection("customers").findOne({}, function (error, res) {
             if (error) throw error;
@@ -47,7 +58,7 @@ function firstTimeout() {
         });
 
 
-        dbase.collection("customers").find({}).toArray(function (error, res) {
+        dbase.collection("customers").find({}).limit(5).toArray(function (error, res) {
             if (error) throw error;
             console.log("Select all obj-s");
             console.log(res);
@@ -59,12 +70,21 @@ function firstTimeout() {
             console.log("Sort example");
             console.log(res);
         });
+
         let query = {address: "Address"};
         dbase.collection("customers").deleteOne(query, function (error, res) {
             if (error) throw error;
             console.log("Delete 1 obj");
             console.log(JSON.stringify(res));
         });
+
+        query = {name: "Home"};
+        let newValues = { $set: {"name": "Mickey", "address": "Canyon 123" } };
+        dbase.collection("customers").updateOne(query, newValues, function (error, res) {
+            if (error) throw error;
+            console.log("1 document updated");
+        });
+
         db.close();
     });
 }
@@ -93,12 +113,62 @@ function thirdTimeout() {
         console.log("Connected");
 
         let dbase = db.db("mydb");
+        dbase.collection("customers", function (error, collection) {
+            if (error) throw error;
+            collection.remove({},function(error, removed){
+                if (error) throw error;
+                console.log(removed);
+            });
+        });
         //Problem operation, db close throw exception after this, but code executed
         // dbase.collection("customers").drop();
         db.close();
     });
 }
 
+function fourthTimeout() {
+    MongoClient.connect(url, function (error, db) {
+        if (error) throw error;
+        console.log("Connected");
+
+        let dbase = db.db("mydb");
+        dbase.createCollection("orders", function (error, res) {
+            if (error) throw error;
+            console.log("Collection orders created!");
+            console.log(res);
+        });
+
+        insertOrder(dbase, 154, 1);
+
+        db.close();
+    });
+}
+
+function fifthTimeout() {
+    MongoClient.connect(url, function (error, db) {
+        if (error) throw error;
+        console.log("Connected");
+
+        let dbase = db.db("mydb");
+
+        dbase.createCollection("products", function (error, res) {
+            if (error) throw error;
+            console.log("Collection products created!");
+            console.log(res);
+        });
+
+        try {
+            dbase.collection("products").update({_id: 154, name: 'Chocolate Heaven'}, {upsert: true});
+            dbase.collection("products").update({_id: 155, name: 'Tasty Lemons'}, {upsert: true});
+            dbase.collection("products").update({_id: 156, name: 'Vanilla Dreams'}, {upsert: true});
+        } catch (ignore) {
+        }
+        db.close();
+    });
+}
+
 setTimeout(firstTimeout, 0);
-setTimeout(secondTimeout, 3000);
-setTimeout(thirdTimeout, 6000);
+setTimeout(secondTimeout, 1500);
+setTimeout(thirdTimeout, 3000);
+setTimeout(fourthTimeout, 4500);
+setTimeout(fifthTimeout, 6000);
