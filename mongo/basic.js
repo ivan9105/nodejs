7b2bf8676551c7f5e61246aac11ca15269ea3e34
ivan9105 <ivan9105@bk.ru>
2018-01-14 14:11:45 +0400

    mongo 3

--- mongo/basic.js
+++ mongo/basic.js
@@ -21,65 +21,84 @@ function insertOneUsers(dbase, random) {
     });
 }
 
-MongoClient.connect(url, function(error, db) {
-    if (error) throw error;
-    console.log("Connected");
-
-    let dbase = db.db("mydb");
-    dbase.createCollection("customers", function(error, res) {
+function firstTimeout() {
+    MongoClient.connect(url, function (error, db) {
         if (error) throw error;
-        console.log("Collection customer created!");
-        console.log(res);
+        console.log("Connected");
+
+        let dbase = db.db("mydb");
+        dbase.createCollection("customers", function (error, res) {
+            if (error) throw error;
+            console.log("Collection customer created!");
+            console.log(res);
+        });
+
+        insertOneCustomers(dbase, true);
+        insertOneCustomers(dbase, true);
+        insertOneCustomers(dbase, true);
+        insertOneCustomers(dbase, true);
+        insertOneCustomers(dbase, true);
+        insertOneCustomers(dbase, false);
+
+        dbase.collection("customers").findOne({}, function (error, res) {
+            if (error) throw error;
+            console.log("Select 1 obj");
+            console.log(res);
+        });
+
+
+        dbase.collection("customers").find({}).toArray(function (error, res) {
+            if (error) throw error;
+            console.log("Select all obj-s");
+            console.log(res);
+        });
+
+        let sort = {_id: 1};
+        dbase.collection("customers").find().sort(sort).toArray(function (error, res) {
+            if (error) throw error;
+            console.log("Sort example");
+            console.log(res);
+        });
+        let query = {address: "Address"};
+        dbase.collection("customers").deleteOne(query, function (error, res) {
+            if (error) throw error;
+            console.log("Delete 1 obj");
+            console.log(JSON.stringify(res));
+        });
+        db.close();
     });
+}
 
-    //Todo sep transaction
-    // dbase.createCollection("users", function(error, res) {
-    //     if (error) throw error;
-    //     console.log("Collection users created!");
-    //     console.log(res);
-    // });
-
-    insertOneCustomers(dbase, true);
-    insertOneCustomers(dbase, true);
-    insertOneCustomers(dbase, true);
-    insertOneCustomers(dbase, true);
-    insertOneCustomers(dbase, true);
-    insertOneCustomers(dbase, false);
-    /Todo sep transaction
-    // insertOneUsers(dbase, true);
-
-    dbase.collection("customers").findOne({}, function (error, res) {
+function secondTimeout() {
+    MongoClient.connect(url, function (error, db) {
         if (error) throw error;
-        console.log("Select 1 obj");
-        console.log(res);
+        console.log("Connected");
+
+        let dbase = db.db("mydb");
+        dbase.createCollection("users", function (error, res) {
+            if (error) throw error;
+            console.log("Collection users created!");
+            console.log(res);
+        });
+
+        insertOneUsers(dbase, true);
+
+        db.close();
     });
+}
 
-
-    dbase.collection("customers").find({}).toArray(function (error, res) {
+function thirdTimeout() {
+    MongoClient.connect(url, function (error, db) {
         if (error) throw error;
-        console.log("Select all obj-s");
-        console.log(res);
+        console.log("Connected");
+
+        let dbase = db.db("mydb");
+        //Problem operation, db close throw exception after this, but code executed
+        // dbase.collection("customers").drop();
+        db.close();
     });
+}
 
-    let sort = { _id: 1 };
-    dbase.collection("customers").find().sort(sort).toArray(function (error, res) {
-        if (error) throw error;
-        console.log("Sort example");
-        console.log(res);
-    });
-
-    let query = { address: "Address"};
-    dbase.collection("customers").deleteOne(query, function (error, res) {
-        if (error) throw error;
-        console.log("Delete 1 obj");
-        console.log(JSON.stringify(res));
-    });
-
-    /Todo sep transaction
-    // dbase.collection("customers").drop(function (error, res) {
-    //    if (error) throw error;
-    //    if (res) console.log("Collection customers deleted");
-    // });
-
-    db.close();
-});
+setTimeout(firstTimeout, 0);
+setTimeout(secondTimeout, 3000);
+setTimeout(thirdTimeout, 6000);
